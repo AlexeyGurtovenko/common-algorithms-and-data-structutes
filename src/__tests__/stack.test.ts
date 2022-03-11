dccd3d28e44c05f754d4c09aa9e2db758056e6f6
Alexey Gurtovenko <alexey.gurtovenko@gmail.com>
2022-03-11 21:30:44 +0700

    covered Stack implementation with tests

---
+++ src/__tests__/stack.test.ts
@@ -0,0 +1,98 @@
+import { IStack, Stack, StackNullableException, StackOverflowException, StackUnderflowException, } from '../data-structures/01_stack/implementation';
+
+describe('Stack test cases', () => {
+
+    let stack: IStack<number>;
+
+    beforeEach(() => {
+        stack = new Stack<number>();
+    });
+
+    test('check if stack is empty upon creation', () => {
+        expect(stack.isEmpty).toBe(true);
+    });
+
+    test('check if push inserts an item at the top of the stack', () => {
+        // Arrange
+        const testValues = [1, 2, 3, 4, 5];
+        const testValuesLength = testValues.length - 1;
+
+        // Act
+        testValues.map(num => stack.push(num));
+
+        // Assert
+        expect(stack.isEmpty).toBe(false);
+        expect(stack.top()).toBe(testValues[testValuesLength]);
+    });
+
+    test('check if stack throws Nullable exception when nullable value is pushed', () => {
+        // Arrange
+
+        // Act
+        const pushNull = () => stack.push(null as any as number);
+        const pushUndefined = () => stack.push(undefined as any as number);
+
+        // Assert
+        expect(pushNull).toThrow(new StackNullableException());
+        expect(pushUndefined).toThrow(new StackNullableException());
+    });
+
+    test('check if the stack is full, then it throws an Overflow exception on push', () => {
+        // Arrange
+        new Array(13).fill(1).map(num => stack.push(num));
+
+        // Act
+        const pushOverTheTop = () => stack.push(1);
+
+        // Assert
+        expect(pushOverTheTop).toThrow(new StackOverflowException())
+    });
+
+    test('check if pop deletes an item from the top of the stack and returns it', () => {
+        // Arrange
+        const testValues = [1, 2, 3, 4, 5];
+        const testValuesLength = testValues.length - 1;
+        testValues.map(num => stack.push(num));
+
+        // Act
+        const poppedItem = stack.pop();
+
+        // Assert
+        expect(stack.top()).toBe(testValues[testValuesLength - 1]);
+        expect(poppedItem).toBe(testValues[testValuesLength]);
+    });
+
+    test('check if the stack is empty, then it throws an Underflow exception on pop', () => {
+        // Arrange
+
+        // Act
+        const popFromEmptyStack = () => stack.pop();
+
+        // Assert
+        expect(popFromEmptyStack).toThrow(new StackUnderflowException())
+    });
+
+    test('check if clear removes all elements from stack', () => {
+        // Arrange
+        const testValues = [1, 2, 3, 4, 5];
+        testValues.map(num => stack.push(num));
+
+        // Act
+        stack.clear();
+
+        // Assert
+        expect(stack.isEmpty).toBe(true);
+    });
+
+    test('check if top returns the top element of the stack without removing it', () => {
+        // Arrange
+        const testValues = [1, 2, 3, 4, 5];
+        const testValuesLength = testValues.length - 1;
+        testValues.map(num => stack.push(num));
+
+        // Act
+
+        // Assert
+        expect(stack.top()).toBe(testValues[testValuesLength]);
+    });
+});
