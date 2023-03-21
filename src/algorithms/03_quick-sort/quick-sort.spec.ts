cfa34bf871010e34cf9478f12bf19d3709ba0b5f
Alexey Gurtovenko <alexey.gurtovenko@gmail.com>
2023-03-21 21:42:02 +0700

    Covered quick sort algorithm with tests

---
+++ src/algorithms/03_quick-sort/quick-sort.spec.ts
@@ -0,0 +1,47 @@
+import { quickSort } from "./implementation";
+
+const testCases = [
+  {
+    // #1
+    input: [],
+    expectedOutput: [],
+  },
+  {
+    // #2
+    input: [1],
+    expectedOutput: [1],
+  },
+  {
+    // #3
+    input: [2, 1],
+    expectedOutput: [1, 2],
+  },
+  {
+    // #4
+    input: [2, 1, 3],
+    expectedOutput: [1, 2, 3],
+  },
+  {
+    // #5
+    input: [2, 1, 3, 5, 4],
+    expectedOutput: [1, 2, 3, 4, 5],
+  },
+  {
+    // #6
+    input: [-2, 1, -3, -5, -4],
+    expectedOutput: [-5, -4, -3, -2, 1],
+  },
+  {
+    // #6
+    input: [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, -1],
+    expectedOutput: [-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+  },
+];
+
+describe("Quick sort test cases", () => {
+  testCases.forEach((item, index) => {
+    test(`test case #${index}`, () => {
+      expect(quickSort(item.input)).toStrictEqual(item.expectedOutput);
+    });
+  });
+});
